719974a3d4422c00ab75986b85140f826f86e408
perrypedro <pedroperryresende@gmail.com>
2025-03-07 13:33:17 -0300

    comments

--- config.py
+++ config.py
@@ -2,7 +2,16 @@ import numpy as np
 from random import uniform
 from math import pi, cos, sin
 
+
+
 def table(n: int) -> np.array:
+    """ Cria uma tabela com n individuos
+        A primeira linha é toda de zeros
+        Cada linha contém informações sobre um indivíduo
+        [X Y θ v]
+        posição, ângulo, velocidade
+    """
+
     tab = np.zeros((1, 4))
     for i in range(n):
         X, Y = uniform(0, 800), uniform(0,800)
@@ -15,13 +24,18 @@ def table(n: int) -> np.array:
     return tab
 
 def get_color(vel: float) -> tuple:
+    """ Retorna valores RGB dado a velocidade
+    """
     return (60, 160, vel*255)
 
 def walk(pos: tuple, theta: float, vel: float) -> (tuple):
+    """ Retorna a nova posição dada a inicial, a velocidade e a direção 
+    """
     X, Y = pos
-    
+
     dX = vel * cos(theta)
     dY = vel * sin(theta)
 
     new_pos = (X + dX, Y + dY)
     return new_pos
+
